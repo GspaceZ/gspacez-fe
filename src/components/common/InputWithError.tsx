5bb8fdd4dbdf2bd031cf6a262918d581aeb1a697
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-06-17 16:27:51 +0700

    feat: signup form

---
+++ src/components/common/InputWithError.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const InputWithError = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex flex-col items-start w-full">{children}</div>
+}
+
+export default InputWithError
