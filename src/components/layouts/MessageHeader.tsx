86b9d28d7262bce229c49a46972858e05b57c3d9
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-10-17 15:03:43 +0700

    feat: message layout

---
+++ src/components/layouts/MessageHeader.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Avatar } from '@nextui-org/react'
+
+interface MessageHeaderProps {
+  avatar?: string
+}
+
+const MessageHeader = (props: MessageHeaderProps) => {
+  return (
+    <div className="sticky top-0 z-10 flex h-[80px] border-b border-gray-200 bg-white shadow-md">
+      <Avatar src={props.avatar} className="fixed left-0 ml-4 mt-5" />
+      <span className="mx-auto my-auto text-3xl font-extrabold">Message</span>
+    </div>
+  )
+}
+
+export default MessageHeader
