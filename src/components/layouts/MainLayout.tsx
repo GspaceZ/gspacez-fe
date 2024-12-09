5a0f4feeee1bd9229bcfb00154ab701b2ce648bd
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2024-12-10 01:07:52 +0700

    feat: add loading screen (#96)

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -10,6 +10,8 @@ import { useState } from 'react'
 import Overlay from '../common/Overlay'
 import { MainLayoutProps } from '@/types/props/layouts'
 import { IconBell, IconSend, IconSettings, IconTrendingUp } from '@tabler/icons-react'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 const MainLayout = ({ children, title }: MainLayoutProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -60,36 +62,38 @@ const MainLayout = ({ children, title }: MainLayoutProps) => {
   return (
     <div className="relative min-h-screen">
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div
-        className={`flex flex-1 flex-col transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'ml-[300px]' : ''}`}
-      >
-        <Overlay isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <Header title={title} isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <main className={`lg:ml-[-300px] ${isSidebarOpen ? 'hidden lg:block' : ''}`}>
-          {children}
-        </main>
-        <TrendingSidebar
-          posts={trendingPostsData}
-          trendingPeople={trendingPeopleData}
-          isVisible={isTrendingSidebarOpen}
-        />
+      <Suspense fallback={<Loading />}>
         <div
-          className={`fixed bottom-0 left-0 right-0 flex items-center justify-around bg-white px-2 py-1 shadow-lg transition-transform duration-300 ease-in-out ${
-            isSidebarOpen ? 'translate-y-full' : 'translate-y-0'
-          } lg:hidden`}
+          className={`flex flex-1 flex-col transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'ml-[300px]' : ''}`}
         >
-          <div className="mx-2 flex w-full justify-between">
-            {buttonOptions.map((buttonOption, index) => (
-              <ButtonOption
-                key={index}
-                button={buttonOption}
-                onClick={() => handleButtonOptionClick(index)}
-                isActive={selectedButtonIndex === index}
-              />
-            ))}
+          <Overlay isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+          <Header title={title} isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+          <main className={`lg:ml-[-300px] ${isSidebarOpen ? 'hidden lg:block' : ''}`}>
+            {children}
+          </main>
+          <TrendingSidebar
+            posts={trendingPostsData}
+            trendingPeople={trendingPeopleData}
+            isVisible={isTrendingSidebarOpen}
+          />
+          <div
+            className={`fixed bottom-0 left-0 right-0 flex items-center justify-around bg-white px-2 py-1 shadow-lg transition-transform duration-300 ease-in-out ${
+              isSidebarOpen ? 'translate-y-full' : 'translate-y-0'
+            } lg:hidden`}
+          >
+            <div className="mx-2 flex w-full justify-between">
+              {buttonOptions.map((buttonOption, index) => (
+                <ButtonOption
+                  key={index}
+                  button={buttonOption}
+                  onClick={() => handleButtonOptionClick(index)}
+                  isActive={selectedButtonIndex === index}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   )
 }
