8d1f96ef6e4a9067d794e795b135b482dff04685
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-12-09 18:05:22 +0700

    feat: add loading screen

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -4,12 +4,16 @@ import * as React from 'react'
 import { AuthLayoutProps } from '@/types/props/layouts'
 import FImage from '../common/FImage'
 import Logo from '@/public/logo.png'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <div className="mt-[100px] flex w-full flex-col items-center">
       <FImage alt="logo" src={Logo.src} className="w-[235px]" />
-      <div>{children}</div>
+      <Suspense fallback={<Loading />}>
+        <div>{children}</div>
+      </Suspense>
     </div>
   )
 }
