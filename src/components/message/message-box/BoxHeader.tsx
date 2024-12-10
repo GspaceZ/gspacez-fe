f30a7defcd9113ec5db7a256a0cb2aace07747e7
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2024-12-11 00:51:12 +0700

    feat: add mobile back button for message box (#99)

--- src/components/message/message-box/BoxHeader.tsx
+++ src/components/message/message-box/BoxHeader.tsx
@@ -4,7 +4,10 @@ import { Avatar, Button } from '@nextui-org/react'
 import { mockUser } from '@/mock/message'
 import { useEffect, useState } from 'react'
 import { IMessageBaseInfo } from '@/types/message'
-import { IconInfoCircle, IconPhoneCall, IconVideo } from '@tabler/icons-react'
+import { IconArrowLeft, IconInfoCircle, IconPhoneCall, IconVideo } from '@tabler/icons-react'
+import { pathWithLocale } from '@/helpers/url/path-with-locale'
+import { usePathname, useRouter } from 'next/navigation'
+import { ROUTE } from '@/utils/constant/route'
 
 interface Props {
   setShowInfo: () => void
@@ -12,15 +15,30 @@ interface Props {
 
 export const BoxHeader = ({ setShowInfo }: Props) => {
   const [messageUser, setMessageUser] = useState<IMessageBaseInfo>()
+  const pathname = usePathname()
+  const router = useRouter()
 
   useEffect(() => {
     setMessageUser(mockUser)
   }, [])
 
+  const handleRedirect = (path: string) => {
+    const destinationPath = pathWithLocale(pathname, path)
+    router.push(destinationPath)
+  }
+
   return (
     <div className="min-h-[60px] w-full border-b border-gray-200">
       <div className="flex h-full w-full items-center justify-between px-4">
         <div className="flex items-center gap-4">
+          <Button
+            isIconOnly
+            variant="light"
+            className="flex md:hidden"
+            onClick={() => handleRedirect(ROUTE.pages.message)}
+          >
+            <IconArrowLeft size={28} />
+          </Button>
           <Avatar src={messageUser?.avatar} alt="Avatar" className="border border-gray-300" />
           <span>{messageUser?.name}</span>
         </div>
