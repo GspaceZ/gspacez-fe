2a4585f71ebc4808ef14085251ebd4503f705206
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-12-12 15:58:28 +0700

    feat: navigate to home on message header button

--- src/components/layouts/message/MessageHeader.tsx
+++ src/components/layouts/message/MessageHeader.tsx
@@ -1,18 +1,33 @@
 'use client'
 
-import { Avatar } from '@nextui-org/react'
+import { pathWithLocale } from '@/helpers/url/path-with-locale'
+import { ROUTE } from '@/utils/constant/route'
+import { Avatar, Button } from '@nextui-org/react'
+import { IconArrowLeft } from '@tabler/icons-react'
+import { usePathname, useRouter } from 'next/navigation'
 
 interface MessageHeaderProps {
   avatar?: string
 }
 
 const MessageHeader = (props: MessageHeaderProps) => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const handleRedirect = (path: string) => {
+    const destinationPath = pathWithLocale(pathname, path)
+    router.push(destinationPath)
+  }
+
   return (
     <div className="sticky top-0 z-10 flex min-h-[80px] border-b border-gray-200 bg-white shadow-md">
-      <Avatar
-        src={props.avatar}
-        className="fixed left-0 ml-4 mt-5 cursor-pointer border border-gray-300"
-      />
+      <div className="fixed left-0 ml-4 mt-5 flex items-center gap-4">
+        <Avatar src={props.avatar} className="cursor-pointer border border-gray-300" />
+        <Button onClick={() => handleRedirect(ROUTE.pages.home)} variant="light">
+          <IconArrowLeft />
+          Back to home
+        </Button>
+      </div>
       <span className="mx-auto my-auto text-3xl font-extrabold">Message</span>
     </div>
   )
