86cd6a5392361311a768a3ce831166c5adfd3a74
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-12-14 10:01:31 +0700

    feat: api create post

---
+++ src/types/response/post/index.ts
@@ -0,0 +1,33 @@
+export type CreatePostRequestDto = {
+  text: string
+  imageUrls?: string[]
+  videoUrls?: string[]
+  feeling?: string | null
+  hashTags?: string[]
+  privacy?: string | null
+  location?: string | null
+}
+
+export type CreatePostResponseDto = {
+  code: number
+  message: string
+  result: {
+    id: string
+    profileId: string
+    content: {
+      text: string
+      imageUrls: string[] | null
+      videoUrls: string[] | null
+      activity: string | null
+    }
+  }
+  privacy: string
+  hashTags: string[] | null
+  location: string | null
+  feeling: string | null
+  type: string
+  trendingPoint: number
+  createdAt: Date
+  updatedAt: Date
+  hidden: boolean
+}
