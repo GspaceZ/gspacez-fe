88248db4cc0e3ce720b5fab85f29631fa2771ca1
Dung Tran Manh <dungtm@missingcorner.com>
2025-01-03 01:44:04 +0700

    feat: auto continue chatting (#136)

--- src/app/[locale]/gzbot/page.tsx
+++ src/app/[locale]/gzbot/page.tsx
@@ -1,17 +1,21 @@
 'use client'
 
+import { BotMessagesBox } from '@/components/gzbot/BotMessagesBox'
 import MainLayout from '@/components/layouts/MainLayout'
 import { fToast } from '@/helpers/toast'
 import { useBot } from '@/hooks/useBot'
 import { BotCompletionRequestDto } from '@/types/dto/bot'
+import { BotMessage } from '@/types/gzbot'
 import { Button } from '@nextui-org/react'
 import { IconMicrophone } from '@tabler/icons-react'
 import { useLocale } from 'next-intl'
 import { useEffect, useState } from 'react'
 
 const Page = () => {
+  const synth = window.speechSynthesis
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  const recognition = new SpeechRecognition()
   const [chatting, setChatting] = useState<boolean>(false)
-  const [text, setText] = useState<string | undefined>()
   const [language, setLanguage] = useState<string>('')
   const locale = useLocale()
   const languageOptions = {
@@ -19,6 +23,7 @@ const Page = () => {
     vi: 'vi-VN'
   }
   const { textGenerate } = useBot()
+  const [messages, setMessages] = useState<BotMessage[]>([])
 
   useEffect(() => {
     if (locale === 'vi') {
@@ -31,7 +36,22 @@ const Page = () => {
   const generateText = async ({ dto }: { dto: BotCompletionRequestDto }) => {
     const response = await textGenerate({ dto })
     if (response.response.candidates && response.response.candidates.length > 0) {
-      setText(response.response.candidates[0].content.parts[0].text)
+      const answer = response.response.candidates[0].content.parts[0].text
+      const newMessage: BotMessage = { fromUser: false, message: answer }
+      setMessages((prevMessages) => [...prevMessages, newMessage])
+      const utterance = new SpeechSynthesisUtterance(answer)
+      utterance.lang = language
+
+      utterance.onstart = () => {
+        recognition.stop()
+      }
+
+      utterance.onend = () => {
+        recognition.start()
+      }
+
+      synth.speak(utterance)
+
       handleOnChat()
     } else {
       fToast('Error: No candidates found in the response', 'danger')
@@ -40,17 +60,24 @@ const Page = () => {
   }
 
   const handleOnChat = () => {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    const recognition = new SpeechRecognition()
     recognition.lang = language
     recognition.onresult = async (e) => {
       e.preventDefault()
+      const transcript = e.results[0][0].transcript
+      const newMessage: BotMessage = { fromUser: true, message: transcript }
+      setMessages((prevMessages) => [...prevMessages, newMessage])
       generateText({
-        dto: { prompt: e.results[0][0].transcript }
+        dto: { prompt: transcript }
       })
     }
     recognition.start()
   }
+
+  // recognition.onspeechend = () => {
+  //   if (!synth.speaking) {
+  //     setChatting(false)
+  //   }
+  // }
 
   return (
     <MainLayout title={'GZBot'}>
@@ -60,13 +87,18 @@ const Page = () => {
             startContent={!chatting && <IconMicrophone />}
             onPress={() => {
               setChatting(!chatting)
-              handleOnChat()
+              if (chatting) {
+                recognition.stop()
+                synth.cancel()
+              } else {
+                handleOnChat()
+              }
             }}
             color={chatting ? 'danger' : 'primary'}
           >
             {chatting ? 'Stop chatting' : 'Start Chatting with GZBot'}
           </Button>
-          <span className="max-w-[600px] text-center">Text recorded: {text}</span>
+          <BotMessagesBox messages={messages} />
         </div>
       </div>
     </MainLayout>
