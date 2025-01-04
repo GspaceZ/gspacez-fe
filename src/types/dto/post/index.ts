cdd314e1d2ce10f30e6d9a2b63695453d26b6916
caodung27 <cdnh1234567890@gmail.com>
2025-01-04 20:02:29 +0700

    feat: fix close modal after create post and update TogglePostResponseDto

--- src/types/dto/post/index.ts
+++ src/types/dto/post/index.ts
@@ -104,25 +104,7 @@ export type TogglePostResponseDto = {
   code: number
   message: string
   result: {
-    id: string
-    profileId: string
-    content: {
-      text: string
-      imageUrls: string[] | null
-      videoUrls: string[] | null
-      activity: string | null
-    }
-    shares: string | null
-    reacts: string[] | null
-    comments: string | null
-    privacy: string
-    hashTags: string[] | null
-    location: string | null
-    feeling: string | null
-    type: string
-    trendingPoint: number
-    createdAt: Date
-    updatedAt: Date
+    postId: string
     hidden: boolean
   }
 }
