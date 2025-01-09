7db8797783c7ce40e01559de3c8dcbf1936473ba
Dung Tran Manh <dungtm@missingcorner.com>
2025-01-09 20:04:07 +0700

    fix: message header (#158)

--- src/components/message/MessageHeader.tsx
+++ src/components/message/MessageHeader.tsx
@@ -1,33 +1,31 @@
 'use client'
 
-import { ROUTE } from '@/utils/constant/route'
 import { Avatar, Button } from '@nextui-org/react'
-import { IconArrowLeft } from '@tabler/icons-react'
-import { useTranslations } from 'next-intl'
 import { LocaleButton } from '../layouts/LocaleButton'
+import { IconHome } from '@tabler/icons-react'
 import { FLink } from '../common/FLink'
+import { ROUTE } from '@/utils/constant/route'
 
 interface MessageHeaderProps {
   avatar?: string
 }
 
 const MessageHeader = (props: MessageHeaderProps) => {
-  const t = useTranslations('message.header')
-
   return (
-    <div className="sticky top-0 z-10 flex min-h-[80px] items-center justify-between border-b border-gray-200 bg-white shadow-md">
+    <div className="sticky top-0 z-10 flex min-h-[50px] items-center justify-between border-b border-gray-200 bg-white shadow-md">
       <div className="ml-4 flex items-center gap-4">
-        <Avatar src={props.avatar} className="cursor-pointer border border-gray-300" />
-        <FLink path={ROUTE.pages.home}>
-          <Button variant="light">
-            <IconArrowLeft />
-            {t('back_home')}
-          </Button>
-        </FLink>
+        <Avatar src={props.avatar} size="sm" className="cursor-pointer border border-gray-300" />
       </div>
-      <span className="mx-auto my-auto text-3xl font-extrabold">{t('title')}</span>
-      <div className="mr-4">
+      <div className="mr-4 flex items-center gap-2">
         <LocaleButton />
+        <FLink path={ROUTE.pages.home}>
+          <Button
+            isIconOnly
+            variant="light"
+            size="sm"
+            startContent={<IconHome size={20} />}
+          ></Button>
+        </FLink>
       </div>
     </div>
   )
