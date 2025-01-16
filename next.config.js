f2fbb47ad55b53bf708b75203c810553c614fcc0
Dung Tran Manh <dungtm@missingcorner.com>
2025-01-16 19:05:45 +0700

    feat: view post (#171)

--- next.config.js
+++ next.config.js
@@ -6,7 +6,21 @@ const withNextIntl = createNextIntlPlugin()
 const nextConfig = {
   compiler: {
     styledComponents: true
-  }
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        ignored: ['**/node_modules/**', '**/.next/**'],
+      };
+    }
+    if (!dev) {
+      config.cache = {
+        type: 'filesystem',
+        allowCollectingMemory: true,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = withNextIntl(nextConfig)
--- src/components/posts/PostPrivacy.tsx
+++ src/components/posts/PostPrivacy.tsx
@@ -2,13 +2,15 @@
 
 import { ReactNode } from 'react'
 import { FIcon } from '../common/FIcon'
+import { FLink } from '../common/FLink'
 
 interface Props {
   time: string
   privacy: string
+  postId: string
 }
 
-export const PostPrivacy = ({ time, privacy }: Props) => {
+export const PostPrivacy = ({ time, privacy, postId }: Props) => {
   const PrivacyIcons: Record<string, ReactNode> = {
     PUBLIC: <FIcon name="World" size={12} />,
     FRIENDS: <FIcon name="Friend" size={12} />,
@@ -16,10 +18,12 @@ export const PostPrivacy = ({ time, privacy }: Props) => {
   }
 
   return (
-    <div className="flex items-center gap-1">
-      <span>{time}</span>
-      <FIcon name="PointFilled" size={6} />
-      {PrivacyIcons[privacy]}
-    </div>
+    <FLink path={`/post/${postId}`}>
+      <div className="flex items-center gap-1">
+        <span>{time}</span>
+        <FIcon name="PointFilled" size={6} />
+        {PrivacyIcons[privacy]}
+      </div>
+    </FLink>
   )
 }
